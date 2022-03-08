a9491acb71782fd79838e635bb6a64947a5c687e
danyentezari <dany.entezari@gmail.com>
2022-03-08 19:38:26 +0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+//import Card from "./Card";
+import SimpleButton from './SimpleButton.js'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(
+        function() {
+
+            if (loggedIn === true) {
+                alert("User is logged in!")
+            }
+           
+        },
+        [ loggedIn ]
+    );
+
+
+    function login() {
+        setLoggedIn(true);
+    }
+
+    return(
+        <div>
+            <button onClick={login} className='btn btn-primary'>Log In</button>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/MainScreen.js
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import Features from "./Features";
+import Footer from "./Footer";
+
+function MainScreen() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Hero></Hero>
+      <Features></Features>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default MainScreen;
